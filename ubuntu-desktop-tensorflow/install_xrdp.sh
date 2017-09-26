a2dd754b6fe3d1144d451815d452f8ac44f7396a
Dorokhov <dorokhov.vladimir@gmail.com>
2017-09-26 19:10:24 +0300

    Update install_xrdp.sh

--- ubuntu-desktop-tensorflow/install_xrdp.sh
+++ ubuntu-desktop-tensorflow/install_xrdp.sh
@@ -13,7 +13,4 @@ sudo sed -i '/\/etc\/X11\/Xsession/i xfce4-session' /etc/xrdp/startwm.sh
 sudo service xrdp restart
 
 sudo apt-get -y install python3-pip python3-dev python-virtualenv
-virtualenv --system-site-packages -p python3 ~/tensorflow
-source ~/tensorflow/bin/activate
-easy_install -U pip
 pip3 install tensorflow 
